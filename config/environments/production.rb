9c715f39cd2269db39373fd3f13a774d03bb2783
naveen <moranaveen1@gmail.com>
2013-04-19 15:29:24 +0530

    updated small change for prdctn

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,7 +20,8 @@ Blog::Application.configure do
   # Generate digests for assets URLs
   config.assets.digest = true
 
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default_url_options = { :host => 'd123.herokuapp.com' }
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.default :charset => "utf-8"
@@ -81,5 +82,5 @@ Blog::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
-  config.action_mailer.default_url_options = { :host => 'd123.herokuapp.com' }
+
 end
