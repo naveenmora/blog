ba9ee174b71fbc2a7231ab884474191da47ea977
naveen <moranaveen1@gmail.com>
2013-04-20 23:30:49 +0530

    updated email

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -21,13 +21,12 @@ Blog::Application.configure do
   config.action_mailer.default :charset => "utf-8"
 
   config.action_mailer.smtp_settings = {
-    :enable_starttls_auto => true,
     :address => "smtp.gmail.com",
     :port => 587,
     :domain => "gmail.com",
     :authentication => :login,
-    :user_name => 'naveenpostbox@gmail.com',
-    :password => 'naveen1991',
+    :user_name => 'naveenblog6@gmail.com',
+    :password => '10120130',
     :enable_starttls_auto => true
   }
 
